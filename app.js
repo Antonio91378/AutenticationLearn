2f7c0aef234a203404a513e7c4fd34daba60dbb2
Antonio <antoniovictor200@gmail.com>
2021-12-15 16:12:04 -0300

    atualizacao para logar com google

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const saltRounds = 10
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const findOrCreate = require('mongoose-findorcreate')
 const port = 3000
 
 const app = express()
@@ -13,6 +16,16 @@ const app = express()
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  session({
+    secret: 'frase politicamente correta',
+    resave: false,
+    saveUninitialized: false,
+  }),
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
 mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true })
 
 //mongo
@@ -20,60 +33,105 @@ mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true })
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
 })
 
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
+
 const User = new mongoose.model('User', userSchema) // creating a user collection inside the userDB database
+
+passport.use(User.createStrategy())
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user)
+  })
+})
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/secrets',
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile)
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user)
+      })
+    },
+  ),
+)
 
 //routes
 // =====================================
 app.get('/', (req, res) => {
   res.render('home')
 })
+app.get('/logout', (req, res) => {
+  res.render('home')
+})
+// =====================================
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
+app.get(
+  '/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets')
+  },
+)
 // =====================================
 app.get('/login', (req, res) => {
   res.render('login')
 })
 app.post('/login', (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
-
-  User.findOne(
-    {
-      email: username,
-    },
-    (err, foundUser) => {
-      if (err) {
-        console.log(err)
-      } else {
-        if (foundUser) {
-          bcrypt.compare(password, foundUser.password, (err, result) => {
-            if (result === true) {
-              res.render('secrets')
-            }
-          })
-        }
-      }
-    },
-  )
+  const user = new User({
+    username: req.body.username,
+    passworrd: req.body.password,
+  })
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.redirect('/secrets')
+      })
+    }
+  })
 })
 // =====================================
+
 app.get('/register', (req, res) => {
   res.render('register')
 })
+app.get('/secrets', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render('secrets')
+  } else {
+    res.redirect('/login')
+  }
+})
 app.post('/register', (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    })
-    newUser.save((err) => {
-      if (!err) {
-        res.render('secrets')
-      } else {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
         console.log(err)
+        res.redirect('/register')
+      } else {
+        passport.authenticate('local')(req, res, () => {
+          res.redirect('/secrets')
+        })
       }
-    })
-  })
+    },
+  )
 })
 // =====================================
 //others
